e2e8794a4774f46b6d7e71c0a277305f5a416b39
Alistair Strachan <alistair.strachan@imgtec.com>
2014-03-28 18:09:10 -0700

    Update DDK to 1.12@2861798.
    Dropped obsolete egl.cfg; Android hasn't needed this since JB MR2.
    
    Change-Id: I9fc2a3c6e51cb28d95d9f676d5a0bf7fd6d10df9
    Signed-off-by: Alistair Strachan <alistair.strachan@imgtec.com>

--- libGPU/gpu.mk
+++ libGPU/gpu.mk
@@ -1,7 +1,4 @@
-
 GPU_SGX540_LOCAL_PATH := hardware/ingenic/xb4780/libGPU
-
-BOARD_EGL_CFG := $(GPU_SGX540_LOCAL_PATH)/egl/egl.cfg
 
 PRODUCT_COPY_FILES += \
 	$(GPU_SGX540_LOCAL_PATH)/libglslcompiler.so:system/lib/libglslcompiler.so           \
@@ -12,7 +9,9 @@ PRODUCT_COPY_FILES += \
 	$(GPU_SGX540_LOCAL_PATH)/libsrv_init.so:system/lib/libsrv_init.so                   \
 	$(GPU_SGX540_LOCAL_PATH)/libsrv_um.so:system/lib/libsrv_um.so                       \
 	$(GPU_SGX540_LOCAL_PATH)/libusc.so:system/lib/libusc.so                             \
-	$(GPU_SGX540_LOCAL_PATH)/hw/gralloc.xxx.so:system/lib/hw/gralloc.xb4780.so                              \
+	$(GPU_SGX540_LOCAL_PATH)/hw/gralloc.xxx.so:system/lib/hw/gralloc.xb4780.so          \
+	$(GPU_SGX540_LOCAL_PATH)/hw/hwcomposer.xxx.so:system/lib/hw/hwcomposer.xb4780.so    \
+	$(GPU_SGX540_LOCAL_PATH)/hw/memtrack.xxx.so:system/lib/hw/memtrack.xb4780.so        \
 	$(GPU_SGX540_LOCAL_PATH)/egl/libEGL_POWERVR_SGX540_130.so:system/lib/egl/libEGL_POWERVR_SGX540_130.so              \
 	$(GPU_SGX540_LOCAL_PATH)/egl/libGLESv1_CM_POWERVR_SGX540_130.so:system/lib/egl/libGLESv1_CM_POWERVR_SGX540_130.so  \
 	$(GPU_SGX540_LOCAL_PATH)/egl/libGLESv2_POWERVR_SGX540_130.so:system/lib/egl/libGLESv2_POWERVR_SGX540_130.so        \
--- libGPU/hal_public.h
+++ libGPU/hal_public.h
@@ -71,7 +71,7 @@ typedef struct
 	int fd[MAX_SUB_ALLOCS];
 
 #define IMG_NATIVE_HANDLE_NUMINTS \
-	((sizeof(unsigned long long) / sizeof(int)) + 6 + MAX_SUB_ALLOCS + 1)
+	((sizeof(unsigned long long) / sizeof(int)) + 5 + MAX_SUB_ALLOCS + 2)
 	/* A KERNEL unique identifier for any exported kernel meminfo. Each
 	 * exported kernel meminfo will have a unique stamp, but note that in
 	 * userspace, several meminfos across multiple processes could have
@@ -101,6 +101,7 @@ typedef struct
 	unsigned int uiAllocSize[MAX_SUB_ALLOCS];
 	unsigned int uiFlags;
 
+	/* XXX FIXME: Hack from Ingenic */
 	int iStride;
 }
 __attribute__((aligned(sizeof(int)),packed)) IMG_native_handle_t;
@@ -157,10 +158,32 @@ typedef struct IMG_buffer_format_public_t
 	/* YUV output format */
 	int bIsYUVFormat;
 
+	/* TRUE if U/Cb follows Y, FALSE if V/Cr follows Y */
+	int bUVCbCrOrdering;
+
 	/* Utility function for adjusting YUV per-plane parameters */
 	IMG_buffer_format_compute_params_pfn pfnComputeParams;
 }
 IMG_buffer_format_public_t;
+
+#if defined(SUPPORT_ANDROID_MEMTRACK_HAL)
+
+#include <hardware/memtrack.h>
+
+typedef struct
+{
+	/* Base memtrack record, copied to caller */
+	struct memtrack_record	base;
+
+	/* Record type, for filtering cached records */
+	enum memtrack_type		eType;
+
+	/* Process ID, for filtering cached records */
+	pid_t					pid;
+}
+IMG_memtrack_record_public_t;
+
+#endif /* defined(SUPPORT_ANDROID_MEMTRACK_HAL) */
 
 typedef struct IMG_gralloc_module_public_t
 {
@@ -181,6 +204,12 @@ typedef struct IMG_gralloc_module_public_t
 	/* Obtain HAL's registered format list */
 	const IMG_buffer_format_public_t *(*GetBufferFormats)(void);
 
+#if defined(SUPPORT_ANDROID_MEMTRACK_HAL)
+	int (*GetMemTrackRecords)(struct IMG_gralloc_module_public_t const *module,
+							  IMG_memtrack_record_public_t **ppsRecords,
+							  size_t *puNumRecords);
+#endif /* defined(SUPPORT_ANDROID_MEMTRACK_HAL) */
+
 	/* Custom-blit components in lieu of overlay hardware */
 	int (*Blit)(struct IMG_gralloc_module_public_t const *module,
 				buffer_handle_t src,
@@ -191,9 +220,9 @@ typedef struct IMG_gralloc_module_public_t
 				 int w, int h, int x, int y);
 
 	int (*Blit3)(struct IMG_gralloc_module_public_t const *module,
-				 unsigned long long ui64SrcStamp,
-				 unsigned long long ui64DestStamp,
-				 int eSrcRotation, int eDestRotation);
+				 unsigned long long ui64SrcStamp, int iSrcWidth,
+				 int iSrcHeight, int iSrcFormat, int eSrcRotation,
+				 buffer_handle_t dest, int eDestRotation);
 }
 IMG_gralloc_module_public_t;
 
