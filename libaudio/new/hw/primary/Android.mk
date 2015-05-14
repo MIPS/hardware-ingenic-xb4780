f89aac9e119d1f2fda35b4e5ab04d8225a32ce0b
Stevan Marinkovic <stevan.marinkovic@imgtec.com>
2015-05-14 10:45:02 +0200

    Fix audio routing when recording without mic.

--- libaudio/new/hw/primary/Android.mk
+++ libaudio/new/hw/primary/Android.mk
@@ -6,6 +6,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq ($(CI20_AUDIO), true)
+LOCAL_CFLAGS += \
+	-DCI20_AUDIO
+endif
+
 LOCAL_MODULE := audio.primary.xb4780
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_SRC_FILES := audio_hw.c
