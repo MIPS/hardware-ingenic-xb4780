1ad7d21ad620eaae588299fce51c676f8fdee970
Dragan Cecavac <dragan.cecavac@imgtec.com>
2016-06-24 12:21:51 +0200

    Adjust ALSA audio HAL module name and module path
    Change-Id: I72f8f43580ffa215bdc7ded208019cfe12ee19ab
    Signed-off-by: Dragan Cecavac <dragan.cecavac@imgtec.com>

--- libaudio/new/hw/primary/Android.mk
+++ libaudio/new/hw/primary/Android.mk
@@ -16,8 +16,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio.primary.grouper
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := audio.primary.xb4780
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_SRC_FILES := \
 	audio_hw.c
 LOCAL_C_INCLUDES += \
