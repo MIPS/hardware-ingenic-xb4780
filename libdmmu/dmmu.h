1600cd5bfa5a8d3778bbe2a71f2863bc342ad995
Dragan Cecavac <dragan.cecavac@imgtec.com>
2016-09-13 13:19:55 +0200

    Add DMMU support based on Ingenic Debian user-space drivers
    Change-Id: I9061b72830ba8b130f4b2b3adb4e0475203dffc9
    Signed-off-by: Dragan Cecavac <dragan.cecavac@imgtec.com>

--- libdmmu/dmmu.h
+++ libdmmu/dmmu.h
@@ -1,6 +1,35 @@
+/*
+ * Copyright (c) 2015 Ingenic Semiconductor Co.,Ltd. All Rights Reserved.
+ *
+ * Website:www.ingenic.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL PRECISION INSIGHT AND/OR ITS SUPPLIERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Authors:
+ *	  qipengzhen <aric.pzqi@ingenic.com>
+ *    liuyang <king.lyang@ingenic.com>
+ */
+
 #ifndef _JZ_DMMU_H_
 #define _JZ_DMMU_H_
-
 
 __BEGIN_DECLS
 
@@ -18,14 +47,9 @@ __BEGIN_DECLS
 #define DMMU_GET_TLB_PHYS                   _IOWR(DMMU_IOCTL_MAGIC, 0x13, struct dmmu_mem_info)
 #define DMMU_SET_TABLE_FLAG 	            _IOW(DMMU_IOCTL_MAGIC, 0x34, int)
 
-#define VIDEO_TABLE_FLAGE 1
+#define DMMU_DUMP_TLB_TABLE					_IOW(DMMU_IOCTL_MAGIC, 0x41, int)
 
-enum {
-    HAL_PIXEL_FORMAT_BGRX_8888      = 0x1ff,
-    /* suport for YUV420 */
-    HAL_PIXEL_FORMAT_JZ_YUV_420_P       = 0x47700001, // YUV_420_P
-    HAL_PIXEL_FORMAT_JZ_YUV_420_B       = 0x47700002, // YUV_420_P BLOCK MODE
-};
+#define VIDEO_TABLE_FLAGE 1
 
 struct dmmu_mem_info {
 	int size;
@@ -53,7 +77,6 @@ static inline void init_page_count(struct dmmu_mem_info *info)
 	info->page_count = page_count;
 	info->start_offset = (unsigned int)info->vaddr - start;
 	info->end_offset = ((unsigned int)info->vaddr + info->size) - end; 
-        //        ALOGD("<----start_offset: %x, end_offset: %x, page_count: %d\n", info->start_offset, info->end_offset, page_count);
 
 	return;
 }
